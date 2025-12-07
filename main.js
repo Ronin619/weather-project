80dbf1f382e0f2cbab4f1f51a65cdd56d9f63440
Ronin619 <eytam619@gmail.com>
2025-12-07 16:32:14 -0500

    feat: implement renderWeather function

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 import { API_KEY } from "./config.js";
-const currentWeather = document.getElementsByClassName("current-weather");
 
 let weatherData = [];
 
-let renderWeather = (res, iconUrl) => {
+let renderWeather = () => {
+  document.querySelector('#currentWeather').replaceChildren();
 
+  for (let i = 0; i < weatherData.length; i++) {
+    const template = `
+      <div class="weatherProfile">
+      <h2 class="temp">${Math.ceil(weatherData[i].temperature)}°</h2>
+      <h2 class="city">${weatherData[i].cityName}</h2>
+      <h2 class="condition">${weatherData[i].weatherCondition}</h2>
+      </div>
+      <div class="weatherIcon">
+        <img src=${weatherData[i].weatherIcon}>
+      </div>
+    `
+    document.querySelector("#currentWeather").insertAdjacentHTML("beforeend", template);
+  }
 }
+
 
 document.querySelector("#searchBtn").addEventListener("click", (e) => {
    e.preventDefault();
 
-  const search = document.querySelector("#inlineFormInputName").value;
+  let search = document.querySelector("#inlineFormInputName").value;
 
   fetchWeather(search);
-})
+
+  search = "";
+});
 
 let fetchWeather = (city) => {
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`
@@ -24,12 +40,21 @@ let fetchWeather = (city) => {
   })
   .then(res => res.json())
   .then(res => { 
-
-    const icon = res.weather[0].icon;
-
-    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-
-    renderWeather(res, iconUrl);
+    addWeather(res);
   })
 }
 
+let addWeather = (res) => {
+  weatherData = [];
+
+  const weatherProfile = {
+    temperature: res.main.temp,
+    cityName: res.name,
+    weatherCondition: res.weather[0].main,
+    weatherIcon: `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
+  }
+
+  weatherData.push(weatherProfile);
+
+  renderWeather(weatherData);
+}
